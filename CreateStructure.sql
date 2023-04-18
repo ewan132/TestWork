18e70b1d8b10c01fbf40529dd638ee98679b3730
Ewan132 <ewan132@yandex.ru>
2023-04-18 19:25:50 +0300

    Creeted CreateStructure.sql

---
+++ CreateStructure.sql
@@ -0,0 +1,21 @@
+CREATE TABLE dbo.SKU(
+	Id int PRIMARY KEY CLUSTERED  IDENTITY NOT NULL
+	,Code int NOT NULL
+	,[Name]  AS ('S'+[Id]) PERSISTED
+	);
+
+CREATE TABLE dbo.Family(
+	Id int PRIMARY KEY   IDENTITY NOT NULL
+	,SurName varchar(100) NULL
+	,BudgetValue int NULL
+	);
+
+	CREATE TABLE dbo.Basket(
+	Id int PRIMARY KEY  IDENTITY NOT NULL
+	,ID_SKU int NOT NULL REFERENCES dbo.SKU(Id)
+	,ID_Family int REFERENCES dbo.Family(Id)
+	,Quantity int CHECK (Quantity>0)
+	,[Value] int CHECK ([Value] >0)
+	,PurchaseDate date DEFAULT GETDATE()
+	,DiscountValue int 
+	)
