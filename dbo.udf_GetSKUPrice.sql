31e7b0f2bd848e0d0887bc8391db16fc5232c886
Ewan132 <ewan132@yandex.ru>
2023-04-24 16:04:15 +0300

    Created function

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -1,1 +1,19 @@
-create function 
+
+CREATE FUNCTION dbo.udf_GetSKUPrice
+(
+
+	@ID_SKU decimal(18,2)
+)
+RETURNS  decimal(18,2)
+AS
+BEGIN
+		select 
+			@ID_SKU= sum(Quantity)/sum(Value)
+		from dbo.Basket B
+
+
+
+	RETURN @ID_SKU
+
+END
+GO
